03efda604d28bc8ff5c9f13ef7397195540782c3
Rudy Zidan <rz.zidan@gmail.com>
2019-02-16 01:58:28 +0200

    Apply validations on List and Item models, Write test cases for these changes.

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -4,8 +4,24 @@ class ItemTest < ActiveSupport::TestCase
   describe 'Associations' do
     let(:item) { Item.first }
 
-    it 'belongs_to list' do
+    it 'belongs_to a list' do
       assert item.list
+    end
+  end
+
+  describe 'Validations' do
+    it 'should belongs_to a list' do
+      item = Item.create(title: 'Test')
+      assert_equal item.valid?, false
+      assert_equal item.errors.count, 1
+      assert_equal item.errors.messages[:list], ["must exist"]
+    end
+
+    it 'should have a title' do
+      item = Item.create(list: List.first)
+      assert_equal item.valid?, false
+      assert_equal item.errors.count, 1
+      assert_equal item.errors.messages[:title], ["can't be blank"]
     end
   end
 end
--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -3,9 +3,18 @@ require "test_helper"
 class ListTest < ActiveSupport::TestCase
   describe 'Associations' do
     let(:list) { List.first }
-    
+
     it 'has many items' do
       assert_equal list.items.count, 1
+    end
+  end
+
+  describe 'Validations' do
+    it 'should have a name' do
+      list = List.create
+      assert_equal list.valid?, false
+      assert_equal list.errors.count, 1
+      assert_equal list.errors.messages[:name], ["can't be blank"]
     end
   end
 end
